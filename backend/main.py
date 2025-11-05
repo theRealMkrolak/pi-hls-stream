731b96f4ded7964309bfe7158ad3716e350e1274
mkrolak <42621308+theRealMkrolak@users.noreply.github.com>
2025-11-04 21:39:43 -0600

    feat: added fastapi and scripts for streaming the camera

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from picamera2 import Picamera2
+from fastapi.responses import Response
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/hls", StaticFiles(directory="hls"), name="hls")
+
+@app.get("/photo")
+async def get_photo() -> Response:
+    picam2 = Picamera2()
+    return Response(content=picam2.capture_array(), media_type="image/jpeg")
---
+++ scripts/streamconverter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ffmpeg \
+    -v verbose -fflags nobuffer -hwaccel auto \
+    -listen 1 -f flv -i rtmp://0.0.0.0:3334/live/stream \
+    -framerate 30 -codec:v libx264 -preset ultrafast -profile:v high -level:v 4.2 -crf 22 -maxrate 3M -bufsize 6M -an \
+    -f hls -hls_time 2 -hls_list_size 2 -hls_flags independent_segments+delete_segments -hls_segment_type mpegts -master_pl_name master.m3u8 /hls/stream_%v.m3u8
