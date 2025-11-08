a5019fc167de91d94d0568a4d2172a307f471252
mkrolak <42621308+theRealMkrolak@users.noreply.github.com>
2025-11-08 09:47:36 -0600

    feat: fixed scripts

--- scripts/streamconverter.sh
+++ scripts/streamconverter.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Default port if not provided (should match webstream.sh port)
+PORT=${1:-8080}
+
 ffmpeg \
     -v verbose -fflags nobuffer -hwaccel auto \
-    -listen 1 -f flv -i rtmp://0.0.0.0:3334/live/stream \
+    -f mjpeg -i http://127.0.0.1:${PORT}/stream.mjpg \
     -framerate 30 -codec:v libx264 -preset ultrafast -profile:v high -level:v 4.2 -crf 22 -maxrate 3M -bufsize 6M -an \
     -f hls -hls_time 2 -hls_list_size 2 -hls_flags independent_segments+delete_segments -hls_segment_type mpegts -master_pl_name master.m3u8 /hls/stream_%v.m3u8
--- scripts/webstream.sh
+++ scripts/webstream.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+
+# Default port if not provided
+PORT=${1:-8080}
+
+# MJPEG stream server using rpicam-vid
+# The stream will be available at http://localhost:${PORT}/stream.mjpg
 rpicam-vid \
-    --metering average --ev 0.6 --bitrate 3145728 --profile high --level 4.2 --nopreview -t 0 \
+    --metering average --ev 0.6 --bitrate 3145728 --nopreview -t 0 \
     --width 1920 --height 1080 --framerate 30 \
-    --codec libav --libav-format flv -o "rtmp://mystreamer.com:3334/live/stream"
+    --codec mjpeg \
+    --inline \
+    --listen 127.0.0.1 \
+    --port ${PORT} \
+    --timeout 0
